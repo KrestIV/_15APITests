0462d595be52dc440567c0f9aa661c5bfc7347c4
svasenkov <aanher@gmail.com>
2024-01-15 19:36:58 +0100

    added specs

--- src/test/java/tests/LoginExtendedTests.java
+++ src/test/java/tests/LoginExtendedTests.java
@@ -1,10 +1,13 @@
 package tests;
 
 import io.qameta.allure.restassured.AllureRestAssured;
+import io.restassured.RestAssured;
 import models.lombok.LoginBodyLombokModel;
 import models.lombok.LoginResponseLombokModel;
+import models.lombok.MissingPasswordModel;
 import models.pojo.LoginBodyModel;
 import models.pojo.LoginResponseModel;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static helpers.CustomAllureListener.withCustomTemplates;
@@ -13,6 +16,7 @@ import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
 import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static specs.LoginSpec.*;
 
 public class LoginExtendedTests {
  /*
@@ -21,6 +25,11 @@ public class LoginExtendedTests {
     2. Get response { "token": "QpwL5tke4Pnpja7X4" }
     3. Check "token" is "QpwL5tke4Pnpja7X4" and status code 200
   */
+
+    @BeforeAll
+    public static void setUp() {
+        RestAssured.baseURI = "https://reqres.in";
+    }
 
     @Test
     void successfulLoginBadPracticeTest() {
@@ -172,6 +181,48 @@ public class LoginExtendedTests {
 
         step("Check response", ()->
             assertEquals("QpwL5tke4Pnpja7X4", response.getToken()));
+    }
+
+    @Test
+    void successfulLoginWithSpecsTest() {
+        LoginBodyLombokModel authData = new LoginBodyLombokModel();
+        authData.setEmail("eve.holt@reqres.in");
+        authData.setPassword("cityslicka");
+
+        LoginResponseLombokModel response = step("Make request", ()->
+            given(loginRequestSpec)
+                    .body(authData)
+
+            .when()
+                .post()
+
+            .then()
+                .spec(loginResponseSpec)
+                .extract().as(LoginResponseLombokModel.class));
+
+        step("Check response", ()->
+            assertEquals("QpwL5tke4Pnpja7X4", response.getToken()));
+    }
+
+
+    @Test
+    void missingPasswordTest() {
+        LoginBodyLombokModel authData = new LoginBodyLombokModel();
+        authData.setEmail("eve.holt@reqres.in");
+
+        MissingPasswordModel response = step("Make request", ()->
+            given(loginRequestSpec)
+                    .body(authData)
+
+            .when()
+                .post()
+
+            .then()
+                .spec(missingPasswordResponseSpec)
+                .extract().as(MissingPasswordModel.class));
+
+        step("Check response", ()->
+            assertEquals("Missing password", response.getError()));
     }
 
 }
