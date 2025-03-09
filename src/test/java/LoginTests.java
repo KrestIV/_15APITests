93a51661c13d9dadd55487ea04856e19f4b83c7b
Ilya Krestsov <krestsov.iv@yandex.ru>
2025-03-09 16:53:46 +0200

    Tests for HW

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -5,53 +5,10 @@ import static io.restassured.http.ContentType.JSON;
 import static org.hamcrest.Matchers.is;
 
 public class LoginTests {
- /*
-    1. Make request (POST) to https://reqres.in/api/login
-        with body { "email": "eve.holt@reqres.in", "password": "cityslicka" }
-    2. Get response { "token": "QpwL5tke4Pnpja7X4" }
-    3. Check "token" is "QpwL5tke4Pnpja7X4" and status code 200
-  */
 
     @Test
-    void successfulLoginTest() {
-        String authData = "{\"email\": \"eve.holt@reqres.in\", \"password\": \"cityslicka\"}";
-
-        given()
-                .body(authData)
-                .contentType(JSON)
-                .log().uri()
-
-        .when()
-            .post("https://reqres.in/api/login")
-
-        .then()
-            .log().status()
-            .log().body()
-            .statusCode(200)
-            .body("token", is("QpwL5tke4Pnpja7X4"));
-    }
-
-    @Test
-    void unsuccessfulLogin400Test() {
-        String authData = "";
-
-        given()
-                .body(authData)
-                .log().uri()
-
-        .when()
-            .post("https://reqres.in/api/login")
-
-        .then()
-            .log().status()
-            .log().body()
-            .statusCode(400)
-            .body("error", is("Missing email or username"));
-    }
-
-    @Test
-    void userNotFoundTest() {
-        String authData = "{\"email\": \"eveasdas.holt@reqres.in\", \"password\": \"cda\"}";
+    void successLoginTest() {
+        String authData = "{\"email\":\"eve.holt@reqres.in\",\"password\":\"cityslicka\"}";
 
         given()
                 .body(authData)
@@ -64,51 +21,13 @@ public class LoginTests {
                 .then()
                 .log().status()
                 .log().body()
-                .statusCode(400)
-                .body("error", is("user not found"));
+                .statusCode(200)
+                .body("token",is("QpwL5tke4Pnpja7X4"));
     }
 
     @Test
-    void missingPasswordTest() {
-        String authData = "{\"email\": \"eveasdas.holt@reqres.in\"}";
-
-        given()
-                .body(authData)
-                .contentType(JSON)
-                .log().uri()
-
-                .when()
-                .post("https://reqres.in/api/login")
-
-                .then()
-                .log().status()
-                .log().body()
-                .statusCode(400)
-                .body("error", is("Missing password"));
-    }
-
-
-    @Test
-    void missingLoginTest() {
-        String authData = "{\"password\": \"cda\"}";
-
-        given()
-                .body(authData)
-                .contentType(JSON)
-                .log().uri()
-
-                .when()
-                .post("https://reqres.in/api/login")
-
-                .then()
-                .log().status()
-                .log().body()
-                .statusCode(400)
-                .body("error", is("Missing email or username"));
-    }
-    @Test
-    void wrongBodyTest() {
-        String authData = "%}";
+    void badBodyLoginTest() {
+        String authData = "{\"email\":\"eve.holt@reqres.in,\"password\":\"cityslicka\"}";
 
         given()
                 .body(authData)
@@ -124,14 +43,4 @@ public class LoginTests {
                 .statusCode(400);
     }
 
-    @Test
-    void unsuccessfulLogin415Test() {
-        given()
-                .log().uri()
-                .post("https://reqres.in/api/login")
-                .then()
-                .log().status()
-                .log().body()
-                .statusCode(415);
-    }
 }
